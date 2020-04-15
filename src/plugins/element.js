f22730b54251879f8bb7e9e7573d3ce3dca6ce3c
sunxiaoxuan6 <1657394323@qq.com>
2020-04-15 09:14:24 +0800

    20200415

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import Element from 'element-ui'
+import { Button } from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(Element)
+Vue.use(Button)
+// Vue.use(Element)
